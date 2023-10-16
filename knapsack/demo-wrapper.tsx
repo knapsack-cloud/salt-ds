65ae111e582cc50d4ce2dff9b32558f5179d5f7e
Evan Lovely <EvanLovely@users.noreply.github.com>
2023-10-16 16:08:42 -0700

    adding more asset sets

--- knapsack/demo-wrapper.tsx
+++ knapsack/demo-wrapper.tsx
@@ -3,9 +3,12 @@ import { SaltProvider } from "@salt-ds/core";
 
 import "@salt-ds/theme/index.css";
 
+const mode = document.body.getAttribute("mode");
+const density = document.body.getAttribute("density");
+
 const DemoWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SaltProvider>
+    <SaltProvider mode={mode} density={density}>
       <div className="demo-wrapper">{children}</div>
     </SaltProvider>
   );
