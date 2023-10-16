4b5a7f6b07eb9d2f4205acdfd7f908c7c3a56048
Evan Lovely <EvanLovely@users.noreply.github.com>
2023-10-16 16:13:15 -0700

    type checks

--- knapsack/demo-wrapper.tsx
+++ knapsack/demo-wrapper.tsx
@@ -1,12 +1,23 @@
 import * as React from "react";
-import { SaltProvider } from "@salt-ds/core";
+import { SaltProvider, Mode, Density } from "@salt-ds/core";
 
 import "@salt-ds/theme/index.css";
 
 const mode = document.body.getAttribute("mode");
+if (mode !== "light" && mode !== "dark") {
+  throw new Error(`Invalid mode: ${mode}`);
+}
 const density = document.body.getAttribute("density");
+if (
+  density !== "medium" &&
+  density !== "high" &&
+  density !== "low" &&
+  density !== "touch"
+) {
+  throw new Error(`Invalid density: ${density}`);
+}
 
-const DemoWrapper = ({ children }: { children: React.ReactNode }) => {
+const DemoWrapper = ({ children }: { children: React.ReactElement }) => {
   return (
     <SaltProvider mode={mode} density={density}>
       <div className="demo-wrapper">{children}</div>
