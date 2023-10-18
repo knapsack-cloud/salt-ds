2d107ab87258e47695b75dcda2c25a95900ee128
Evan Lovely <EvanLovely@users.noreply.github.com>
2023-10-18 15:56:26 -0400

    setup theming via asset sets

--- knapsack/demo-wrapper.tsx
+++ knapsack/demo-wrapper.tsx
@@ -3,19 +3,19 @@ import { SaltProvider, Mode, Density } from "@salt-ds/core";
 
 import "@salt-ds/theme/index.css";
 
-const mode = document.body.getAttribute("mode");
-if (mode !== "light" && mode !== "dark") {
-  throw new Error(`Invalid mode: ${mode}`);
-}
-const density = document.body.getAttribute("density");
-if (
-  density !== "medium" &&
-  density !== "high" &&
-  density !== "low" &&
-  density !== "touch"
-) {
-  throw new Error(`Invalid density: ${density}`);
-}
+const mode = document.body.getAttribute("data-mode") || "light";
+// if (mode !== "light" && mode !== "dark") {
+//   throw new Error(`Invalid mode: ${mode}`);
+// }
+const density = document.body.getAttribute("data-density") || "medium";
+// if (
+//   density !== "medium" &&
+//   density !== "high" &&
+//   density !== "low" &&
+//   density !== "touch"
+// ) {
+//   throw new Error(`Invalid density: ${density}`);
+// }
 
 const DemoWrapper = ({ children }: { children: React.ReactElement }) => {
   return (
---
+++ ks-theme-create.ts
@@ -0,0 +1,85 @@
+import { join, relative } from "path";
+import fs from "fs";
+import {
+  KnapsackAssetSetsConfig,
+  KnapsackAssetSetConfig,
+} from "@knapsack/types";
+import tokens from "./knapsack/dist/tokens/design-tokens.nested.json";
+
+const cssTokens = fs.readFileSync(
+  join(__dirname, "./knapsack/dist/tokens/design-tokens.css"),
+  "utf8"
+);
+const assetSetsPath = join(
+  __dirname,
+  "./knapsack/data/knapsack.asset-sets.json"
+);
+console.log("Creating theme...");
+const themeNames = Object.keys(tokens.theme);
+
+const themes = themeNames.map((theme) => {
+  console.log(`Making theme ${theme}`);
+  const themeTokenNames = cssTokens.split("\n").flatMap((line) => {
+    if (!line.trim().startsWith(`--theme-${theme}`)) return [];
+    const tokenName = line.trim().split(":")[0];
+    return [
+      `${tokenName.replace(`theme-${theme}`, `salt`)}: var(${tokenName});`,
+    ];
+  });
+  const contents = [".salt-theme.salt-theme {", ...themeTokenNames, "}"].join(
+    "\n"
+  );
+  const cssPath = join(__dirname, `ks-theme-${theme}.css`);
+  fs.writeFileSync(cssPath, contents, {
+    encoding: "utf8",
+  });
+  return {
+    cssPath,
+    themeName: theme,
+  };
+});
+
+const assetSets: KnapsackAssetSetsConfig = {
+  globalAssetSetIds: ["salt-light", "salt-dark", ...themeNames],
+  allAssetSets: Object.fromEntries([
+    [
+      "salt-light",
+      ((): KnapsackAssetSetConfig => ({
+        id: "salt-light",
+        title: "Salt (light)",
+        inlineJs: "document.body.setAttribute('data-mode', 'light');",
+        assets: [],
+      }))(),
+    ],
+    [
+      "salt-dark",
+      ((): KnapsackAssetSetConfig => ({
+        id: "salt-dark",
+        title: "Salt (dark)",
+        inlineJs: "document.body.setAttribute('data-mode', 'dark');",
+        assets: [],
+      }))(),
+    ],
+    ...themes.map(({ cssPath, themeName }) => {
+      const assetSet: KnapsackAssetSetConfig = {
+        id: themeName,
+        title: themeName,
+        assets: [
+          {
+            src: "../dist/tokens/design-tokens.css",
+          },
+          {
+            src: relative(join(__dirname, "./knapsack/data"), cssPath),
+          },
+        ],
+      };
+      return [themeName, assetSet];
+    }),
+  ]),
+};
+
+fs.writeFileSync(
+  join(__dirname, "./knapsack/data/knapsack.asset-sets.json"),
+  JSON.stringify(assetSets, null, 2)
+);
+console.log("Done");
